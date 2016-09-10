d858f7acd910cc4e4339ff633f4752bd25481552
Kostia Grinchik <Kostia Grinchik>
2016-09-10 22:05:18 +0300

    fill node class

--- src/max-heap.js
+++ src/max-heap.js
@@ -2,11 +2,14 @@ const Node = require('./node');
 
 class MaxHeap {
 	constructor() {
-		
+		this.parentNodesCounter = 0;
+		this.root = null;
+		this.parentNodes = [];
 	}
 
 	push(data, priority) {
-		
+		this.insertNode(new Node(data, priority));
+		this.shiftNodeUp(new Node(data, priority));
 	}
 
 	pop() {
@@ -34,7 +37,16 @@ class MaxHeap {
 	}
 
 	insertNode(node) {
-		
+		if(!this.root){
+			this.root = node;	
+		}
+		if(this.parentNodesCounter == 2){
+			this.parentNodesCounter = 0
+			this.parentNodes.shift();
+		}
+		this.parentNodesCounter ++
+		this.parentNodes.push(node)
+
 	}
 
 	shiftNodeUp(node) {
--- src/node.js
+++ src/node.js
@@ -1,22 +1,58 @@
 class Node {
+ 
+
 	constructor(data, priority) {
+		this.data = data;
+		this.priority = priority;
+		this.parent = null;
+		this.left = null;
+		this.right = null;
 
 	}
 
 	appendChild(node) {
-
+		if (this.left && this.right){
+			return;
+		}
+		node.parent = this;
+		if(this.data > node.data){
+			this.left = node;
+		} else{
+			this.right = node; 
+		}
+		
 	}
 
 	removeChild(node) {
-
+		if ((this.left != node && this.right != node)){
+			throw new Error('err');
+		}
+		node.parent = null;
+		if(this.data > node.data){
+			this.left = null;
+		} else{
+			this.right = null; 
+		}
 	}
 
 	remove() {
-
+		if(this.parent){
+			this.parent.removeChild(this);
+		}
 	}
 
 	swapWithParent() {
-		
+		if(this.parent){
+			var pra = this.parent.parent 
+			this.parent.parent = this;
+			this.parent = pra;
+
+			// var me = this.parent.right;
+			// var left =  this.parent.left;
+			// var parent = this.parent;
+			// this.right = parent;
+			// left.parent = me;
+		}
 	}
 }
 
