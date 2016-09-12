f3f809141382a3202ad44e51fcdb99933530cba2
Kostia Grinchik <valushagrinchik@gmail.com>
2016-09-12 10:47:29 +0300

    test_12_09

--- src/max-heap.js
+++ src/max-heap.js
@@ -13,7 +13,18 @@ class MaxHeap {
 	}
 
 	pop() {
-		
+		//var parentNodesTemp = this.parentNodes;
+		//for (var i = 0; i < parentNodesTemp.length-1; i++) {
+		//	for (var j = i+1; j < parentNodesTemp.length; j++) {
+		//		if(parentNodesTemp.priority[i] < parentNodesTemp.priority[j]){
+		//		var temp = parentNodesTemp[i]
+		//		parentNodesTemp[i] = parentNodesTemp[j]
+		//		parentNodesTemp[j] = temp
+		//		}
+		//	}
+		//}
+	//	this.parentNodes = parentNodesTemp;
+	//	this.parentNodes.priority.sort();
 	}
 
 	detachRoot() {
@@ -33,12 +44,17 @@ class MaxHeap {
 	}
 
 	clear() {
-		
+		this.root = null;
+		//this.parentNodes.length = 0;
+		this.parentNodes = [];
 	}
 
 	insertNode(node) {
 		if(!this.root){
-			this.root = node;	
+		//	for (var i = 0; i <= node.length; i++) {
+				this.root = node
+		//	}
+		//	this.root = node;	
 		}
 		if(this.parentNodesCounter == 2){
 			this.parentNodesCounter = 0
@@ -50,6 +66,7 @@ class MaxHeap {
 	}
 
 	shiftNodeUp(node) {
+
 		
 	}
 
