0122e105e5ba4fe41b8eb038fc1f3f51ebfdca3a
Kostia Grinchik <valushagrinchik@gmail.com>
2016-09-12 10:48:19 +0300

    test_12_09

--- src/queue.js
+++ src/queue.js
@@ -2,11 +2,24 @@ const MaxHeap = require('./max-heap.js');
 
 class PriorityQueue {
 	constructor(maxSize) {
+		if(maxSize < 30){
+			this.maxSize = maxSize;
+		} else{
+			this.maxSize = 30;
+		}
+		this.heap = new MaxHeap();
 
 	}
 
 	push(data, priority) {
+		var countSize = 1;
+		if(this.maxSize > countSize){
+			this.heap.push(data,priority);
+			countSize ++;
+		}else{
+			throw new Error('err');
 
+		}
 	}
 
 	shift() {
